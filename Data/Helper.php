79a37a1532d18aecce34b98dc7619c210f27fe17
Bagus Ardin Prayoga <bagusardinp@gmail.com>
2025-11-30 14:13:23 +0700

    Helper

---
+++ Data/Helper.php
@@ -0,0 +1,8 @@
+<?php
+namespace helper;
+function budi(){
+    echo "Nama saya budi" . PHP_EOL;
+}
+
+const APPLICATION = "Belajar_OOP_PHP";
+?>
---
+++ Namespace.php
@@ -0,0 +1,12 @@
+<?php
+require_once "Data/Conflic.php";
+require_once "Data/Helper.php";
+
+$conlict1 = new Data\One\Conflict();
+$conlict2 = new Data\Two\Conflict();
+
+echo helper\APPLICATION . PHP_EOL;
+helper\budi();
+
+
+?>
