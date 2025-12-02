98eaa3e5983610f158f4fe99189f343aef8d77ac
Bagus Ardin Prayoga <bagusardinp@gmail.com>
2025-12-02 12:34:47 +0700

    add var dump

--- Polimorfisme.php
+++ Polimorfisme.php
@@ -3,9 +3,12 @@
 
     $company = new Company();
     $company->programmer = new Programmer("Bagus");
+    var_dump($company);
     $company->programmer->Work();
     $company->programmer = new BackendProgrammer("Bagus");
+    var_dump($company);
     $company->programmer->Work();
     $company->programmer = new FrontendProgrammer("Bagus");
+    var_dump($company);
     $company->programmer->Work();
 ?>
